fb5810b7d4112c5b4164902b1aa28fb1763f15ce
skettzu <99dwong@gmail.com>
2024-03-28 17:52:46 -0400

    FCFS, SPN, and table print working

--- scheduling.c
+++ scheduling.c
@@ -5,7 +5,7 @@
 /*
 Run your simulator for following inputs
 Part 1:
-
+                                                                                    (start - arr)   (finish - arr)
     Process     Arrival Time     Service Time       Start Time      Finish Time     Wait Time       Turnaround Time
     A           0                3                  *               *               *               *
     B           2                6                  *               *               *               *
@@ -24,13 +24,22 @@ Part 2:
 
 /*
 To Do:
-    - Parse input file and store process' name, arrival time, and service time to own struct
+    - Parse input file and store process' name, arrival time, and service time to own struct [Done]
+    - Get FCFS working [Done]
+    - Get SPN working [Done]
+    - Get SRT working
+    - Get Table printing working
+    - Get Gantt Chart printing working
 */
 struct Process {
     // struct for each process
     char name[2];
     int arr_time;
     int serv_time;
+    int start_time;
+    int fin_time;
+    int wait_time;
+    int turn_time;
 };
 
 void parser(struct Process *inp) {
@@ -40,7 +49,7 @@ void parser(struct Process *inp) {
     char str[20];
     int temp;
     int process_ind = 0;
-    fp1 = fopen("part2.txt", "r"); 
+    fp1 = fopen("part1.txt", "r"); 
     while (fgets(str, sizeof(str), fp1) != NULL)
 	{
 		lineP = 0;
@@ -72,12 +81,79 @@ void FCFS(struct Process *inp){
     /*
     Algorithm for First Come First Serve
     */
+   struct Process temp;
+   
+   // Re-order processes to be FCFS
+   for (int i = 0; i<5; i++){
+    if(i!=4){
+        for (int j = i+1; j<5; j++){
+            if (inp[i].start_time > inp[j].start_time){
+                temp = inp[i];
+                inp[i] = inp[j];
+                inp[j] = temp;
+            }
+        }
+    }
+   }
+   int temp_time;
+   // Fill in rest of processes infomation
+   for (int i = 0; i<5; i++){
+        if(i==0){
+            inp[0].start_time = inp[0].arr_time;
+            inp[0].fin_time = inp[0].start_time + inp[0].serv_time;
+            inp[0].wait_time = inp[0].start_time - inp[0].arr_time;
+            inp[0].turn_time = inp[0].fin_time - inp[0].arr_time;
+            temp_time = inp[0].fin_time;
+            printf("%s           %d                %d                  %d               %d               %d               %d\n", inp[i].name, inp[i].arr_time, inp[i].serv_time, inp[i].start_time, inp[i].fin_time, inp[i].wait_time, inp[i].turn_time);   
+            continue;
+        }
+        inp[i].start_time = temp_time;
+        inp[i].fin_time = inp[i].start_time + inp[i].serv_time;
+        inp[i].wait_time = inp[i].start_time - inp[i].arr_time;
+        inp[i].turn_time = inp[i].fin_time - inp[i].arr_time;
+        temp_time = inp[i].fin_time;
+
+        printf("%s           %d                %d                  %d               %d               %d               %d\n", inp[i].name, inp[i].arr_time, inp[i].serv_time, inp[i].start_time, inp[i].fin_time, inp[i].wait_time, inp[i].turn_time);   
+   }
 }
 
 void SPN(struct Process *inp){
     /*
     Algorithm for Shortest Process Next
     */
+   struct Process temp;
+   for (int i = 0; i<5; i++){
+    if(i!=4){
+        for (int j = i+1; j<5; j++){
+            if (inp[i].serv_time > inp[j].serv_time){
+                temp = inp[i];
+                inp[i] = inp[j];
+                inp[j] = temp;
+            }
+        }
+    }
+   }
+   int temp_time;
+   // Fill in rest of processes infomation
+   for (int i = 0; i<5; i++){
+        if(i==0){
+            inp[0].start_time = inp[0].arr_time;
+            inp[0].fin_time = inp[0].start_time + inp[0].serv_time;
+            inp[0].wait_time = inp[0].start_time - inp[0].arr_time;
+            inp[0].turn_time = inp[0].fin_time - inp[0].arr_time;
+            temp_time = inp[0].fin_time;
+            printf("%s           %d                %d                  %d               %d               %d               %d\n", inp[i].name, inp[i].arr_time, inp[i].serv_time, inp[i].start_time, inp[i].fin_time, inp[i].wait_time, inp[i].turn_time);   
+            continue;
+        }
+        inp[i].start_time = temp_time;
+        inp[i].fin_time = inp[i].start_time + inp[i].serv_time;
+        inp[i].wait_time = inp[i].start_time - inp[i].arr_time;
+        inp[i].turn_time = inp[i].fin_time - inp[i].arr_time;
+        temp_time = inp[i].fin_time;
+
+        printf("%s           %d                %d                  %d               %d               %d               %d\n", inp[i].name, inp[i].arr_time, inp[i].serv_time, inp[i].start_time, inp[i].fin_time, inp[i].wait_time, inp[i].turn_time);   
+   }
+   
 }
 
 void SRT(struct Process *inp){
@@ -89,10 +165,12 @@ int main() {
     struct Process inp[5];
     // parse input file and store input to process struct
     parser(inp);
-    
-    printf("%s\n", inp[0].name);
-    printf("%d\n", inp[0].arr_time);
-    printf("%d\n", inp[0].serv_time);
+    printf("Process     Arrival Time     Service Time       Start Time      Finish Time     Wait Time       Turnaround Time\n");
+    //FCFS(inp);
+    SPN(inp);
+    //printf("%s\n", inp[0].name);
+    //printf("%d\n", inp[0].arr_time);
+    //printf("%d\n", inp[0].serv_time);
     
     return 0;
 }
