c5663722c62f4f295fe44583d79e738c8659f72c
skettzu <99dwong@gmail.com>
2024-03-28 09:40:58 -0400

    Lab 7 Init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ELEC4075_Lab7_Scheduling
+Create a simulator in C that simulates First-Come-First Serve (FCFC), Shortest Process Next (SPN), and Shortest-
+Remaining Time (SRT).
+• The simulator will need to accept the following inputs for each process: Process Name, Arrival Time, and
+Service Time – you can create input files or have the user input each time.
+• The simulator would need to calculate and display the following outputs (in a table) for each process: Process
+Name, Arrival Time, Service Time, Start Time, Finish Time, Wait Time, Turnaround Time.
+• Similar to the examples we did in the lecture, you can ignore I/O i.e., in non-preemptive algorithms, the
+processes run straight through to the end once started, and in preemptive algorithms, only the OS can preempt a process.
+
+Create a visual representation (Gantt chart) in C. For example, SPN for Part 1 will produce the following Gantt Chart:
+A A A
+     B B B B B B
+                   C C C C
+                          D D D D D
+                E E 
---
+++ scheduling.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+Run your simulator for following inputs
+Part 1:
+
+    Process     Arrival Time     Service Time       Start Time      Finish Time     Wait Time       Turnaround Time
+    A           0                3                  *               *               *               *
+    B           2                6                  *               *               *               *
+    C           4                4                  *               *               *               *
+    D           6                5                  *               *               *               *
+    E           8                2                  *               *               *               *
+
+Part 2:
+   Process     Arrival Time     Service Time       Start Time      Finish Time     Wait Time       Turnaround Time
+    A           0                8                  *               *               *               *
+    B           1                5                  *               *               *               *
+    C           2                9                  *               *               *               *
+    D           3                2                  *               *               *               *
+    E           4                6                  *               *               *               *
+*/
+
+int main() {
+
+    return 0;
+}
